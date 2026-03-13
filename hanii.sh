7314d6474d05e7f33a333fcb29b71b93f8d565ec
03Anmoll <anmolmulla97@gmail.com>
2026-03-13 16:55:51 +0530

    uploaded .sh file

---
+++ hanii.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+mkdir ~/Awsdeploy
+echo "directory created successfully"
+cd ~/Awsdeploy
+
+sudo apt update
+sudo apt install git -y
+
+pwd
+echo "installed git successfully"
+rm -rf *
+git clone https://github.com/muktapmandolkar/PracticeSpringBoot.git main
+echo "clone the repository successfully"
+
+sudo apt update
+sudo apt install nginx
+echo "installed nginx successfully"
+sudo systemctl enable nginx
+sudo systemctl start nginx
+echo "status of nginx is in running state"
+
+sudo apt install openjdk-17-jdk -y
+echo "jave installed successfully"
+
+sudo apt install maven -y
+echo "Maven installed successfully"
+
+sudo apt update
+sudo apt install mysql-server -y
+echo "installing mysql"
+sudo systemctl enable mysql
+sudo systemctl start mysql
+sudo systemctl status mysql
+echo "status of the mysql is in running state"
+
+cd ~/Awsdeploy/main/Parag_StudentMgmtSystem
+
+mvn clean install -DskipTests
+echo "Build successfully"
+
+echo "configuring database"
+
+PROPERTIES_FILE=~/aleena/main/Parag_StudentMgmtSystem/src/main/resources/application.properties
+
+DB_URL=$(grep "spring.datasource.url" $PROPERTIES_FILE | cut -d'=' -f2)
+DB_USER=$(grep "spring.datasource.username" $PROPERTIES_FILE | cut -d'=' -f2)
+DB_PASS=$(grep "spring.datasource.password" $PROPERTIES_FILE | cut -d'=' -f2)
+DB_NAME=$(echo $DB_URL | awk -F'/' '{print $NF}')
+
+echo $DB_NAME 
+echo $DB_USER
+echo $DB_PASS
+
+
+#DB_NAME="studentdb"
+#DB_USER="parag"
+#DB_PASS="password123"
+
+sudo mysql -u root -p <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+
+FLUSH PRIVILEGES;
+
+EOF
+
+echo "mysql database and user configured successfully"
+
+cd ~/Awsdeploy/main/Parag_StudentMgmtSystem/target
+pwd
+java -jar Student-Management-1.0.0.jar
+echo "executing the jar file successfully" 
